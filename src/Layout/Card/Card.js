60c3c4a98683d44dc83d42d0ef96eb879b0ef120
toannguyen0312 <80366569+toannguyen0312@users.noreply.github.com>
2023-08-09 13:40:31 -0700

    update all

--- src/Layout/Card/Card.js
+++ src/Layout/Card/Card.js
@@ -4,8 +4,8 @@ function Card({ card, numberOfCard, cardNum}) {
 return (
     <div className="card">
         <div className="card-body">
-            <h5 className="card-title">Card {cardNum} of{numberOfCard}</h5>
-            <h6 className="card-text"></h6>
+            <h5 className="card-title">Card {cardNum} of {numberOfCard}</h5>
+            <h6 className="card-text">{card.front}</h6>
             <button type="button" className="btn btn-secondary"></button>
         </div>
     </div>
