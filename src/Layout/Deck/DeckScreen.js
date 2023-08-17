72e6cbff708adfa6438a6b21c568e5dff1c080c5
Toan <tony.nguyen.1105@gmail.com>
2023-08-17 07:45:54 -0700

    redo ScreenDeck

--- src/Layout/Deck/DeckScreen.js
+++ src/Layout/Deck/DeckScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { readDeck, readCard } from "../../utils/api";
+import { readDeck, readCard, deleteDeck } from "../../utils/api";
 import { useParams, Link } from "react-router-dom";
 import CardScreen from "../Card/CardScreen";
 
@@ -18,6 +18,13 @@ useEffect(() => {
     loadDeck()
 },[deckId]);
 
+const handleDelete = async(deckId) =>{
+    const result = window.confirm("Are you sure you want to delete this deck?");
+    if(result){
+        deleteDeck(deckId).then(history.go("/"))
+    }       
+}
+
 return (
     <div>
         <nav arial-label="bread-crumb">
@@ -30,9 +37,9 @@ return (
         </nav>
         <h5>{deck.name}</h5>
         <p>{deck.description}</p>
-        <button type="button" className="btn btn-secondary">Edit</button>
-        <button type="button" className="btn btn-primary">Study</button>
-        <button type="button" className="btn btn-primary">Add Cards</button>
+        <Link to={`/decks/${deckId}/edit`}><button type="button" className="btn btn-secondary">Edit</button></Link>
+        <Link to={`/decks/${deckId}/study`}><button type="button" className="btn btn-primary">Study</button></Link>
+        <Link to={`/decks/${deckId}/cards/${cardId}/edit`}><button type="button" className="btn btn-primary">Add Cards</button></Link>
         <button type="button" className="btn btn-danger">Delete</button>
         <h3>Cards </h3>
         <CardScreen cards={cards}/>
